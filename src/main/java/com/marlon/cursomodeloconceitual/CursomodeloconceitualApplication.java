20b386a2d9917ed2c91a53682ac5991ffa7dfdd8
Marlon Marques <marlon.belohd@GMAIL.COM>
2019-06-08 23:23:20 -0300

    Projeto criado, REST testado

---
+++ src/main/java/com/marlon/cursomodeloconceitual/CursomodeloconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.marlon.cursomodeloconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodeloconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodeloconceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlon/cursomodeloconceitual/CursomodeloconceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marlon.cursomodeloconceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursomodeloconceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
