f45a1a6e49a073ce41f412d0fa839c3f3e419345
Marlon Marques <marlon.belohd@GMAIL.COM>
2019-06-15 11:42:22 -0300

    Criando operacao de instanciacao

--- src/main/java/com/marlon/cursomodeloconceitual/CursomodeloconceitualApplication.java
+++ src/main/java/com/marlon/cursomodeloconceitual/CursomodeloconceitualApplication.java
@@ -1,13 +1,32 @@
 package com.marlon.cursomodeloconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomodeloconceitualApplication {
+import com.marlon.cursomodeloconceitual.domain.Categoria;
+import com.marlon.cursomodeloconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomodeloconceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodeloconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 
 }
