e894b5f9e1a9c9c6a248666e965fe7a114ae3668
Marlon Marques <marlon.belohd@GMAIL.COM>
2019-06-26 17:42:39 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/marlon/cursomodeloconceitual/resources/ClienteResource.java
+++ src/main/java/com/marlon/cursomodeloconceitual/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.marlon.cursomodeloconceitual.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marlon.cursomodeloconceitual.domain.Cliente;
+import com.marlon.cursomodeloconceitual.dto.ClienteDTO;
 import com.marlon.cursomodeloconceitual.services.ClienteService;
 
 @RestController
@@ -22,5 +31,41 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) { // o id acima faz referencia com o de baixo
 		Cliente obj = service.find(id);	 //vai buscar o id passado
 		return ResponseEntity.ok().body(obj); //retorna ok o obj
+	}
+	
+	//Alterar
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE) // pegar pelo id
+	// relacao com HTTP
+	public ResponseEntity<Void> delete(@PathVariable Integer id) { // o id acima faz referencia com o de baixo
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET) // pegar pelo id
+	// relacao com HTTP
+	public ResponseEntity<List<ClienteDTO>> findAll() { // o id acima faz referencia com o de baixo
+		List<Cliente> list = service.findAll();	 //vai buscar o id passado
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); //percorre a lista , mapea em cada objeto da lista criando um objeto dto e volta para o tipo lista 
+		return ResponseEntity.ok().body(listDto); //retorna ok o obj
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET) // pegar pelo id
+	// relacao com HTTP
+	public ResponseEntity<Page<ClienteDTO>> findAll(
+			@RequestParam(value="page", defaultValue="0")Integer page, // valor padrão 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction) { // o id acima faz referencia com o de baixo, defaultValue = (ascendente ou descendente)
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);	 //vai buscar o id passado
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj)); 
+		return ResponseEntity.ok().body(listDto); //retorna ok o obj
 	}
 }
--- src/main/java/com/marlon/cursomodeloconceitual/services/ClienteService.java
+++ src/main/java/com/marlon/cursomodeloconceitual/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.marlon.cursomodeloconceitual.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marlon.cursomodeloconceitual.domain.Cliente;
+import com.marlon.cursomodeloconceitual.dto.ClienteDTO;
 import com.marlon.cursomodeloconceitual.repositories.ClienteRepository;
+import com.marlon.cursomodeloconceitual.services.exceptions.DataIntegrityException;
 import com.marlon.cursomodeloconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +29,42 @@ public class ClienteService {
 		}
 		return obj;
 	}
+	
+	public Cliente update(Cliente obj) { // quando o id não é nulo, ele atualiza
+		Cliente newObj = find(obj.getId()); // verifica se existe
+		updateData(newObj, obj); // atualiza esse dado com base no objeto como argumento
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	// paginção: mostrar uma quantidade especifica
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+	
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//a partir de um dto
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(),objDto.getEmail(), null,null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 }
