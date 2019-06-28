9ae4ec3a8b04e4fe2e7bd3ca7e8df937de39b781
Marlon Marques <marlon.belohd@GMAIL.COM>
2019-06-28 18:11:02 -0300

    Criando profile de teste

---
+++ src/main/java/com/marlon/cursomodeloconceitual/config/TestConfig.java
@@ -0,0 +1,28 @@
+// configrações especificas do profile de teste
+
+package com.marlon.cursomodeloconceitual.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.marlon.cursomodeloconceitual.services.DBService;
+
+// tudo que estiver dentro da classe so será ativado quando o profile de teste for true
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
