b8a2839d8a2ccb40950c477f57c05555dd1b6959
Marlon Marques <marlon.belohd@gmail.com>
2019-07-14 10:02:05 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/marlon/cursomodeloconceitual/CursomodeloconceitualApplication.java
+++ src/main/java/com/marlon/cursomodeloconceitual/CursomodeloconceitualApplication.java
@@ -1,13 +1,17 @@
 package com.marlon.cursomodeloconceitual;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marlon.cursomodeloconceitual.services.S3Service;
+
 @SpringBootApplication
 public class CursomodeloconceitualApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodeloconceitualApplication.class, args);
@@ -15,6 +19,7 @@ public class CursomodeloconceitualApplication implements CommandLineRunner {
 
 	@Override // metodo de injeção
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\MARLON BELO MARQUES\\Pictures\\ippo.png");
 	}
 
 }
